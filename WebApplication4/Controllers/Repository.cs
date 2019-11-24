c27f98dea8bc02e8c171ea527b10f9d7c34e7a76
avinashnarra <avinashnarra69@gmail.com>
2019-11-24 14:18:53 -0500

    added service cofigurations in startup.cs and appdbcontect files

---
+++ WebApplication4/Controllers/Repository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks; 
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class Repository:IRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        
+
+        public Repository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+    }
+}
--- WebApplication4/Startup.cs
+++ WebApplication4/Startup.cs
@@ -6,8 +6,10 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using WebApplication4.Models;
 
 namespace WebApplication4
 {
@@ -23,12 +25,14 @@ namespace WebApplication4
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
+            services.AddDbContext<AppDbContext>(options
+                =>options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            //services.Configure<CookiePolicyOptions>(options =>
+            //{
+            //    // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+            //    options.CheckConsentNeeded = context => true;
+            //    options.MinimumSameSitePolicy = SameSiteMode.None;
+            //});
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
